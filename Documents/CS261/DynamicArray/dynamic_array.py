e8d00c135a4b8761891cc6b8a9851d9b6ce93fea
lucas Rouchy <lucasrouchy1@gmail.com>
2020-10-11 14:47:28 -0700

    Capacity

--- Documents/CS261/DynamicArray/dynamic_array.py
+++ Documents/CS261/DynamicArray/dynamic_array.py
@@ -1,7 +1,9 @@
 # DynamicArray: An array that grows to accommodate new elements.
 # Your implementation should pass the tests in test_dynamic_array.py.
 # YOUR NAME
-
+DEFAULT_CAPACITY = 10
 class DynamicArray:
     def __init__(self):
         self.data = []
+        self.capacity = DEFAULT_CAPACITY
+        

ecd20b200bfed2c885932153e605e23812b16679
lucas Rouchy <lucasrouchy1@gmail.com>
2020-10-11 16:40:25 -0700

    Appending

--- Documents/CS261/DynamicArray/dynamic_array.py
+++ Documents/CS261/DynamicArray/dynamic_array.py
@@ -1,20 +1,33 @@
 # DynamicArray: An array that grows to accommodate new elements.
 # Your implementation should pass the tests in test_dynamic_array.py.
 # YOUR NAME
+# import numpy
+import numpy 
+
 DEFAULT_CAPACITY = 10
 class DynamicArray:
     def __init__(self):
-        self.data = []
+        
         self.capacity = DEFAULT_CAPACITY
         self.size = 0
+        self.data = numpy.ndarray(self.capacity,dtype= object)
+ 
+
     
+    def __len__(self):
+        lenght= 0
+        for object in self.data:
+            if object !=None:
+                lenght +=1
+        return lenght
 
     def is_empty(self):
-        return self.data == []
-
-    
-    def __len__(self) -> int:
-        return self.size
-    # def append(self, value):
-    #     index = self.size
-    #     self.data.insert(index, value)
+        if self.__len__() == 0: 
+            return True
+        else:
+            return False
+    def append(self, value):
+        index = self.__len__()
+        print("appending: ", index)
+        self.data[index] = object
+        
